a5efc0bab6bbb2b8d9ae5e32f06fa6c3efb0008a
JakkaSanthosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-03-04 10:01:56 +0530

    Time: 2 ms (44.47%) | Memory: 41.8 MB (37.14%) - LeetSync

--- 2589-maximum-value-of-a-string-in-an-array/maximum-value-of-a-string-in-an-array.java
+++ 2589-maximum-value-of-a-string-in-an-array/maximum-value-of-a-string-in-an-array.java
@@ -8,7 +8,7 @@ class Solution {
                 // Try parsing the whole string as an integer
                 int num = Integer.parseInt(str);
                 ans = Math.max(ans, num);
-            } catch (NumberFormatException e) {
+            } catch (Exception e) {
                 // If it's not a valid integer, take the length of the string
                 ans = Math.max(ans, str.length());
             }
