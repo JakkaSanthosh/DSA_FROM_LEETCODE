cb555f16cd946b81dd8d8d90068462128ee746b6
JakkaSanthosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-03-08 07:42:45 +0530

    Time: 4 ms (10.90%) | Memory: 41.7 MB (40.60%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,55 +1,55 @@
-// class Solution {
-//     public int minimumRecolors(String b, int k) {
-//         int ans=0;
-//         Map<Character,Integer> mp=new HashMap<>();
-//         for(int i=0;i<k;i++){
-//           char c=b.charAt(i);
-//           mp.put(c,mp.getOrDefault(c,0)+1);
-//         }
-//         ans=mp.get('W');
-//         for(int i=1;i<b.length()-k;i++){
-//             char x=b.charAt(i-1);
-//             char y=b.charAt(i+k-1);
-//             mp.put(y,mp.getOrDefault(y,0)+1);
-//             mp.put(x,mp.getOrDefault(x,0)-1);
-//             if(mp.get(x)<=0) mp.remove(x);
-//             ans=Math.min(ans,mp.getOrDefault('W',0));
-//         }
-//         return ans;
-//     }
-// }
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-public int minimumRecolors(String b, int k) {
-int ans;
-Map<Character, Integer> mp = new HashMap<>();
-
-// Initialize the first window
-for (int i = 0; i < k; i++) {
-char c = b.charAt(i);
-mp.put(c, mp.getOrDefault(c, 0) + 1);
+    public int minimumRecolors(String b, int k) {
+        int ans=0;
+        Map<Character,Integer> mp=new HashMap<>();
+        for(int i=0;i<k;i++){
+          char c=b.charAt(i);
+          mp.put(c,mp.getOrDefault(c,0)+1);
+        }
+        ans=mp.getOrDefault('W',0);
+        for(int i=1;i<b.length()-k+1;i++){
+            char x=b.charAt(i-1);
+            char y=b.charAt(i+k-1);
+            mp.put(y,mp.getOrDefault(y,0)+1);
+            mp.put(x,mp.getOrDefault(x,0)-1);
+            if(mp.get(x)<=0) mp.remove(x);
+            ans=Math.min(ans,mp.getOrDefault('W',0));
+        }
+        return ans;
+    }
 }
+// import java.util.HashMap;
+// import java.util.Map;
 
-ans = mp.getOrDefault('W', 0); // Ensure we handle cases where 'W' might not exist
+// class Solution {
+// public int minimumRecolors(String b, int k) {
+// int ans;
+// Map<Character, Integer> mp = new HashMap<>();
 
-// Sliding window
-for (int i = 1; i <= b.length() - k; i++) {
-char x = b.charAt(i - 1);    // Character going out of the window
-char y = b.charAt(i + k - 1); // Character coming into the window
+// // Initialize the first window
+// for (int i = 0; i < k; i++) {
+// char c = b.charAt(i);
+// mp.put(c, mp.getOrDefault(c, 0) + 1);
+// }
 
-// Remove left character
-mp.put(x, mp.get(x) - 1);
-if (mp.get(x) == 0) mp.remove(x);
+// ans = mp.getOrDefault('W', 0); // Ensure we handle cases where 'W' might not exist
 
-// Add right character
-mp.put(y, mp.getOrDefault(y, 0) + 1);
+// // Sliding window
+// for (int i = 1; i <= b.length() - k; i++) {
+// char x = b.charAt(i - 1);    // Character going out of the window
+// char y = b.charAt(i + k - 1); // Character coming into the window
 
-// Update answer
-ans = Math.min(ans, mp.getOrDefault('W', 0));
-}
+// // Remove left character
+// mp.put(x, mp.get(x) - 1);
+// if (mp.get(x) == 0) mp.remove(x);
 
-return ans;
-}
-}
+// // Add right character
+// mp.put(y, mp.getOrDefault(y, 0) + 1);
+
+// // Update answer
+// ans = Math.min(ans, mp.getOrDefault('W', 0));
+// }
+
+// return ans;
+// }
+// }
