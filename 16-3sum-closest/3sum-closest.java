442f6bf1c5efec0d16d54cdb5ea6a88d95193bb5
JakkaSanthosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-03-13 20:13:10 +0530

    Time: 565 ms (6.40%) | Memory: 45.7 MB (5.65%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,36 +1,25 @@
-import java.util.Arrays;
-import java.util.TreeMap;
-
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        TreeMap<Integer, Integer> mp = new TreeMap<>();
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            int l = i + 1, h = nums.length - 1;
-            while (l < h) {
-                int s = nums[i] + nums[l] + nums[h];
-
-                // If we find the exact match, return immediately
-                if (s == target) return s;
-
-                int diff = Math.abs(target - s);
-
-                // Update only if this sum is smaller (or preferred)
-                mp.putIfAbsent(diff, s); // Add only if not present
-                if (mp.get(diff) > s) {
-                    mp.put(diff, s); // Replace only if the sum is smaller
-                }
-
-                // Move pointers
-                if (s < target) {
-                    l++;
-                } else {
-                    h--;
-                }
+       TreeMap<Integer,Integer> mp=new TreeMap<>();
+       Arrays.sort(nums);
+       for(int i=0;i<nums.length-2;i++){
+        int l=i+1,h=nums.length-1;
+        while(l<h){
+            int s=nums[i]+nums[l]+nums[h];
+            if(s==target) return s;
+            else if(s<target){
+                l++;
             }
+            else {
+                h--;
+               }
+            int key=s<target?target-s:s-target;
+             if (!mp.containsKey(key) || mp.get(key) > s) {
+                    mp.put(key, s);
+                }
+           
         }
-
-        return mp.firstEntry().getValue();  // Return the closest sum
+       } 
+     return mp.firstEntry().getValue(); 
     }
 }
