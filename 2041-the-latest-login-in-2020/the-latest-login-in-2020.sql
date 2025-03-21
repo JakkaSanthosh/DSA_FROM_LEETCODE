0bb5bc3a4d012a6523d81bea327629b03c54f4a9
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-03-21 19:32:44 +0530

    Time: 1241 ms (49.53%) | Memory: 0B (100.00%) - LeetSync

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select distinct user_id,max(time_stamp) as last_stamp from logins where time_stamp between '2020-01-01 00:00:00' and '20-12-31 23:59:59' group by user_id 
+select user_id,max(time_stamp) as last_stamp from logins where time_stamp between '2020-01-01 00:00:00' and '20-12-31 23:59:59' group by user_id 
