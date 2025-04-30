3c8740e8f6dc283c48f81330f5bfff747ba619ce
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-05-01 00:35:50 +0530

    Time: 15 ms (57.03%) | Memory: 45.2 MB (60.73%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,29 +1,18 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         Arrays.sort(piles);
-        int left = 1;
-        int right = piles[piles.length-1];
-        int answer = right;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            long totalHours = 0;  // use long to avoid overflow
-
-            for (int pile : piles) {
-                // Inline ceiling calculation without ceil function
-                totalHours += (pile + (long)mid - 1) / mid;
-            }
-
-            if (totalHours <= h) {
-                answer = mid;
-                right = mid - 1; // try slower speed
-            } else {
-                left = mid + 1;  // try faster speed
-            }
+        int low = 1, high = piles[piles.length - 1], res = high;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long reqHours = 0;
+            for (int i : piles)
+                reqHours += (i + (long) mid - 1) / mid;
+            if (reqHours <= h) {
+                res = mid;
+                high = mid - 1;
+            } else
+                low = mid + 1;
         }
-
-        return answer;
+        return res;
     }
-
-   
 }
