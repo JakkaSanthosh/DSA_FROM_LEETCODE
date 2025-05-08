e49573f7c321d4cdf23d6fffcbddaf913913fd12
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-05-08 14:35:48 +0530

    Time: 6 ms (6.46%) | Memory: 41.6 MB (32.02%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -1,9 +1,23 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        Set<Integer> hs=new HashSet<>();
-        for(var i: nums){
-            if(i!=0) hs.add(i);
-        }
-        return hs.size();
+       Queue<Integer> pq=new PriorityQueue<>();
+       int c=0,zc=0,x=0;
+       for(var i:nums){
+        if(i!=0) pq.add(i);
+        else zc++;
+       }
+       while(zc!=nums.length){
+        if(!pq.isEmpty())
+           x=pq.poll();
+           pq.clear();
+           for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+              nums[i]-=x; 
+              if(nums[i]==0) zc++;
+              else pq.offer(nums[i]);
+           }}
+           c++;
+       }
+       return c;
     }
 }
