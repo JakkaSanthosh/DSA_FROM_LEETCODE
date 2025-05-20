1e3b61fa5e3da0d830713dbb6bde2ef489a3c3b7
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-05-20 11:07:32 +0530

    Time: 28 ms (52.15%) | Memory: 44.7 MB (20.13%) - LeetSync

---
+++ 1844-maximum-number-of-balls-in-a-box/maximum-number-of-balls-in-a-box.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countBalls(int low, int high) {
+        Map<Integer,Integer> mp=new HashMap<>();
+        int max=0;
+        while(low<=high){
+            insert(mp,low);
+            //insert(mp,high);
+            low++;
+           // high--;
+        }
+        for(var i:mp.values()){
+            if(i>max) max=i;
+        }
+        return max;
+    }
+    static void insert(Map<Integer,Integer> mp,int num){
+        int sum=0;
+        while(num!=0){
+            int r=num%10;
+             sum+=r;
+             num/=10;
+        }
+        mp.put(sum,mp.getOrDefault(sum,0)+1);
+    }
+}
