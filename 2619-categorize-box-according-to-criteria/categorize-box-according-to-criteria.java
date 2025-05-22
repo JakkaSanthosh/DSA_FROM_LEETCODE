a04897d9e192d61538876b696a55c53644d02a3a
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-05-22 15:12:51 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (94.14%) - LeetSync

--- 2619-categorize-box-according-to-criteria/categorize-box-according-to-criteria.java
+++ 2619-categorize-box-according-to-criteria/categorize-box-according-to-criteria.java
@@ -1,6 +1,6 @@
 class Solution {
     public String categorizeBox(int length, int width, int height, int mass) {
-        long volume=1L *length*width*height;
+        long volume=(long)length*width*height;
         String cat="";
         if(length>=10000||width>=10000||height>=10000||mass>=10000||volume>=1000000000) cat="Bulky";
       if(mass>=100&&cat.equals("Bulky")) return "Both";
