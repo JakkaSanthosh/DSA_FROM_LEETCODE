b0d038a0e60d264649ea1c16ff4a677acfbaf105
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-06-15 12:40:39 +0530

    Time: 10 ms (100.00%) | Memory: 45.4 MB (100.00%) - LeetSync

--- 3893-generate-tag-for-video-caption/generate-tag-for-video-caption.java
+++ 3893-generate-tag-for-video-caption/generate-tag-for-video-caption.java
@@ -6,16 +6,11 @@ class Solution {
         StringBuilder sb=new StringBuilder();
         sb.append('#');
         for(int i=0;i<a.length;i++){
-            if(i==0){
-                sb.append(Character.toLowerCase(a[i].charAt(0)));
-                sb.append(a[i].substring(1).toLowerCase());
-            }
-            else{
-                sb.append(Character.toUpperCase(a[i].charAt(0)));
-                 sb.append(a[i].substring(1).toLowerCase());
-            }
+            char ch= (i==0)?Character.toLowerCase(a[i].charAt(0)):Character.toUpperCase(a[i].charAt(0));
+            sb.append(ch);
+                sb.append(a[i].substring(1).toLowerCase());       
         }
-        if(sb.length()>100) return sb.substring(0,100).toString();
-        return sb.toString();
+       return sb.length()>100 ?sb.substring(0,100).toString():sb.toString();
+       
     }
 }
