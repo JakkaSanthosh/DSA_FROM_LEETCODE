706851712b7b6353d171b20d5d7ff5eb4c7ccfe9
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-06-23 17:26:53 +0530

    Time: 13 ms (7.08%) | Memory: 47.9 MB (6.65%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,27 +1,27 @@
 class Solution {
     public int[][] merge(int[][] interval) {
-        List<Integer> inner=new ArrayList<>();
-        List<List<Integer>> outer=new ArrayList<>();
-        Arrays.sort(interval, (a, b) -> Integer.compare(a[0], b[0]));
+        List<Integer> inner = new ArrayList<>();
+        List<List<Integer>> outer = new ArrayList<>();
+        Arrays.sort(interval, (a, b) -> {return a[0]-b[0];});
         inner.add(interval[0][0]);
-        int max=interval[0][1];
-        for(int i=1;i<interval.length;i++){ 
-           
-                if(interval[i][0]>max) {inner.add(max);
+        int max = interval[0][1];
+        for (int i = 1; i < interval.length; i++) {
+            if (interval[i][0] > max) {
+                inner.add(max);
                 outer.add(inner);
-                inner=new ArrayList<>();
+                inner = new ArrayList<>();
                 inner.add(interval[i][0]);
-                max=interval[i][1];}
-                max=Math.max(max,interval[i][1]);
-            
+                max = interval[i][1];
+            }
+            max = Math.max(max, interval[i][1]);
         }
         inner.add(max);
         outer.add(inner);
-        int a[][]=new int[outer.size()][2];
+        int a[][] = new int[outer.size()][2];
         System.out.println(outer);
-        for(int i=0;i<outer.size();i++){
-            a[i][0]=outer.get(i).get(0);
-            a[i][1]=outer.get(i).get(1);
+        for (int i = 0; i < outer.size(); i++) {
+            a[i][0] = outer.get(i).get(0);
+            a[i][1] = outer.get(i).get(1);
         }
         return a;
     }
