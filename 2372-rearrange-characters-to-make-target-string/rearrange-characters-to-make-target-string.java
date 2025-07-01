3a4fe7f6b80d386c52d28fe7ddbd52d821892563
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-07-01 08:58:58 +0530

    Time: 2 ms (40.66%) | Memory: 41.8 MB (26.10%) - LeetSync

---
+++ 2372-rearrange-characters-to-make-target-string/rearrange-characters-to-make-target-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int rearrangeCharacters(String s, String target) {
+        int min=99999999;
+        if(target.length()>s.length()) return 0;
+        if(target.equals(s)) return 1;
+        Map<Character,Integer> mp=new HashMap<>();
+        for(var i:s.toCharArray()){
+           mp.put(i,1+mp.getOrDefault(i,0));
+        }
+        Map<Character,Integer> mp1=new HashMap<>();
+        for(var i:target.toCharArray()){
+           mp1.put(i,1+mp1.getOrDefault(i,0));
+        }
+       for(char i:target.toCharArray()){
+        if(!mp.containsKey(i)) return 0;
+        else min=Math.min(min,mp.get(i)/mp1.get(i));
+       }
+       return min;
+    }
+}
