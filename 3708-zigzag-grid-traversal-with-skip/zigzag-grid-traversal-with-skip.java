b1f0bec4b526a6f6538a1bb05e96d9b1ddeb0777
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-07-28 11:27:01 +0530

    Time: 1 ms (100.00%) | Memory: 45.6 MB (69.72%) - LeetSync

---
+++ 3708-zigzag-grid-traversal-with-skip/zigzag-grid-traversal-with-skip.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> zigzagTraversal(int[][] grid) {
+        List<Integer> al = new ArrayList<>();
+        for (int i = 0; i < grid.length; i++) {
+            if (i % 2 == 0)
+                for (int j = 0; j < grid[0].length; j += 2)
+                    al.add(grid[i][j]);
+            else {
+                if (grid[0].length % 2 == 0)
+                    for (int j = grid[0].length - 1; j >= 0; j -= 2)
+                        al.add(grid[i][j]);
+                else
+                    for (int j = grid[0].length - 2; j >= 0; j -= 2)
+                        al.add(grid[i][j]);
+            }
+        }
+        return al;
+    }
+}
