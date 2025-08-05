8eef3f209313b895019a9115a92b7f4e1a59fabb
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-08-05 10:21:10 +0530

    Time: 131 ms (5.04%) | Memory: 46.8 MB (10.57%) - LeetSync

---
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countMaxOrSubsets(int[] nums) {
+        int cnt=0,n=nums.length;
+        List<Integer> al=new ArrayList<>();
+        for(int i=1;i<(1<<n);i++){
+            int ans=0;
+            for(int j=0;j<n;j++){
+                if(checkBit(i,j)) ans|=nums[j];
+            }
+            al.add(ans);
+        }
+        if(al.isEmpty()) return 0;
+        Collections.sort(al);
+        int max=al.get(al.size()-1);
+        for(int i=al.size()-1;i>=0;i--){
+            if(al.get(i)==max) cnt++;
+            else break;
+        }
+        return cnt;
+    }
+    static boolean checkBit(int n,int i){
+        return (n&(1<<i))!=0;
+    }
+}
