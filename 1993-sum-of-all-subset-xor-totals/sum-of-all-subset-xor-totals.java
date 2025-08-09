93612a6df7de677fdd28473a098ef348d722c7be
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-08-09 06:59:23 +0530

    Time: 6 ms (17.84%) | Memory: 41.1 MB (52.20%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,16 +1,17 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        int sum=0,n=nums.length;
-        for(int i=1;i<(1<<n);i++){
-            int xor=0;
-            for(int j=0;j<n;j++){
-                if(checkBit(i,j)) xor^=nums[j];
+        int sum = 0, n = nums.length;
+        for (int i = 1; i < (1 << n); i++) {
+            int xor = 0;
+            for (int j = 0; j < n; j++) {
+                if (checkBit(i, j))
+                    xor ^= nums[j];
             }
-            sum+=xor;
+            sum += xor;
         }
         return sum;
     }
-    static boolean checkBit(int n,int i){
-        return (n&(1<<i))>0;
+    static boolean checkBit(int n, int i) {
+        return (n & (1 << i)) > 0;
     }
 }
