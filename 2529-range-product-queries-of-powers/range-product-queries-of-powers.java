ac02c1963eed5d4901582714feb2af9b353a6a8e
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-08-11 18:24:41 +0530

    Time: 19 ms (78.49%) | Memory: 106.4 MB (5.38%) - LeetSync

---
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] productQueries(int n, int[][] queries) {
+        List<Integer> al = new ArrayList<>();
+        int c = 0;
+        while (n != 0) {
+            if (checkBit(n, 0)) {
+                al.add((int) Math.pow(2, c));
+            }
+            c++;
+            n >>= 1;
+        }
+        int a[] = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            long prod = 1;
+            int low = queries[i][0], high = queries[i][1];
+            while (low <= high) {
+                prod = (prod * al.get(low++)) % ((int) 1e9 + 7);
+            }
+            a[i] = (int) prod;
+        }
+        return a;
+    }
+
+    static boolean checkBit(int n, int i) {
+        return (n & 1) != 0;
+    }
+}
