882b4f66d0911902170a64945ca696a179784e47
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-08-18 07:52:13 +0530

    Time: 3 ms (41.11%) | Memory: 45.3 MB (61.23%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -1,29 +1,36 @@
 class Solution {
     public List<Integer> luckyNumbers(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int rowMin[]=new int[m];
-        int colMax[]=new int[n];
-        for(int i=0;i<m;i++){
-            int min=Integer.MAX_VALUE;
-            for(int j=0;j<n;j++){
-                 min=Math.min(min,matrix[i][j]);
-            }
-            rowMin[i]=min;
-        }
-        for(int i=0;i<n;i++){
-            int max=Integer.MIN_VALUE;
-            for(int j=0;j<m;j++){
-                max=Math.max(matrix[j][i],max);
-            }
-            colMax[i]=max;
-        }
-        List<Integer> al=new ArrayList<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(rowMin[i]==colMax[j]) al.add(matrix[i][j]);
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int rowMin[] = preCompute(matrix, m, n, 0);
+        int colMax[] = preCompute(matrix, n, m, 1);
+        List<Integer> al = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rowMin[i] == colMax[j])
+                    al.add(matrix[i][j]);
             }
         }
         return al;
+    }
+
+    static int[] preCompute(int matrix[][], int m, int n, int k) {
+        int a[] = new int[m];
+        for (int i = 0; i < m; i++) {
+            if (k == 0) {
+                int min = Integer.MAX_VALUE;
+                for (int j = 0; j < n; j++) {
+                    min = Math.min(matrix[i][j], min);
+                }
+                a[i] = min;
+            } else {
+                int max = Integer.MIN_VALUE;
+                for (int j = 0; j < n; j++) {
+                    max = Math.max(matrix[j][i], max);
+                }
+                a[i] = max;
+            }
+        }
+        return a;
     }
 }
