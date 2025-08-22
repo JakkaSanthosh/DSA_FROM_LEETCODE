d2074f0c41bb22795ecb2b3a999c53521c68ca03
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-08-22 14:21:03 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (74.33%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cs
+++ 58-length-of-last-word/length-of-last-word.cs
@@ -1,7 +1,11 @@
 public class Solution {
     public int LengthOfLastWord(string s) {
         s=s.Trim();
-        string[] a=s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-        return a[a.Length-1].Length;
+        int cnt=0;
+        for(int i=s.Length-1;i>=0;i--){
+            if(s[i]!=' ') cnt++;
+            else break;
+        }
+       return cnt;
     }
 }
