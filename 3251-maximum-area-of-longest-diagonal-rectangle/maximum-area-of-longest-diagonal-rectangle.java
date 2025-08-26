69ed7635e588d19d8745e97451bf72314a56dfb5
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-08-26 08:07:41 +0530

    Time: 1 ms (96.00%) | Memory: 44.5 MB (62.77%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.java
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.java
@@ -5,11 +5,11 @@ class Solution {
         for (int i = 0; i < dimensions.length; i++) {
             int len = dimensions[i][0];
             int wid = dimensions[i][1];
-            if (Math.sqrt(len * len + wid * wid) > diag) {
-                maxArea =  len * wid ;
+            if (Math.sqrt(len * len + wid * wid) >= diag) {
+                if(Math.sqrt(len * len + wid * wid)==diag) maxArea=Math.max(maxArea,len*wid);
+                else maxArea =  len * wid ;
                 diag =Math.sqrt(len * len + wid * wid);
             }
-            else if(Math.sqrt(len * len + wid * wid)==diag) maxArea=Math.max(maxArea,len*wid);
         }
         return maxArea;
     }
