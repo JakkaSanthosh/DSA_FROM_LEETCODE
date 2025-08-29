b78da77a9ca0a37fd5e9932595d8fc8aca0492a9
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-08-29 07:40:57 +0530

    Time: 6 ms (44.17%) | Memory: 46.1 MB (42.60%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> mp = new HashMap<>();
+        for (int i = 0; i < numCourses; i++)
+            mp.put(i, new ArrayList<>());
+        int indegree[] = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+            int u = prerequisites[i][1];
+            int v = prerequisites[i][0];
+            mp.get(u).add(v);
+            indegree[v]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0)
+                q.offer(i);
+        }
+        int res[] = new int[numCourses], idx = 0;
+        while (!q.isEmpty()) {
+            res[idx++] = q.peek();
+            int node = q.poll();
+            for (var i : mp.get(node)) {
+                indegree[i]--;
+                if (indegree[i] == 0)
+                    q.offer(i);
+            }
+        }
+        return (idx == numCourses) ? res : new int[] {};
+    }
+}
