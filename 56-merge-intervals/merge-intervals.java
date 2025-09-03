5c079b9282eb2f0c79301f62039ac955646dcd49
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-09-03 21:12:30 +0530

    Time: 9 ms (44.17%) | Memory: 46.6 MB (56.57%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,23 +2,28 @@ class Solution {
     public int[][] merge(int[][] interval) {
         List<Integer> inner = new ArrayList<>();
         List<List<Integer>> outer = new ArrayList<>();
-        Arrays.sort(interval, (a, b) -> {return a[0]-b[0];});
-        inner.add(interval[0][0]);
-        int max = interval[0][1];
+        Arrays.sort(interval, (a, b) -> {
+            if (a[0] != b[0])
+                return a[0] - b[0];
+            return a[1] - b[1];
+        });
+        int start = interval[0][0], finish = interval[0][1];
         for (int i = 1; i < interval.length; i++) {
-            if (interval[i][0] > max) {
-                inner.add(max);
+            if (interval[i][0] <= finish) {
+                finish = Math.max(finish, interval[i][1]);
+            } else {
+                inner.add(start);
+                inner.add(finish);
                 outer.add(inner);
+                start = interval[i][0];
+                finish = interval[i][1];
                 inner = new ArrayList<>();
-                inner.add(interval[i][0]);
-                max = interval[i][1];
             }
-            max = Math.max(max, interval[i][1]);
         }
-        inner.add(max);
+        inner.add(start);
+        inner.add(finish);
         outer.add(inner);
         int a[][] = new int[outer.size()][2];
-        System.out.println(outer);
         for (int i = 0; i < outer.size(); i++) {
             a[i][0] = outer.get(i).get(0);
             a[i][1] = outer.get(i).get(1);
