b8c5a4205f7eb49f497f8e5a7fbfeba41d9166ba
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-09-19 10:20:11 +0530

    Time: 109 ms (89.08%) | Memory: 56.1 MB (50.70%) - LeetSync

--- 3797-design-spreadsheet/design-spreadsheet.java
+++ 3797-design-spreadsheet/design-spreadsheet.java
@@ -1,44 +1,32 @@
 class Spreadsheet {
-    int mat[][];
-
+    Map<String,Integer> mp;
+    int rows;
     public Spreadsheet(int rows) {
-        mat = new int[rows][26];
+        this.rows=rows;
+        mp=new HashMap<>();
     }
-
+    
     public void setCell(String cell, int value) {
-        int col = cell.charAt(0) - 'A';
-        int row = Integer.parseInt(cell.substring(1))-1;
-        mat[row][col] = value;
-
+        mp.put(cell,value);
     }
-
+    
     public void resetCell(String cell) {
-        int col = cell.charAt(0) - 'A';
-        int row = Integer.parseInt(cell.substring(1))-1;
-        mat[row][col] = 0;
+       mp.remove(cell);
     }
-
+    
     public int getValue(String formula) {
         int idx = formula.indexOf('+');
-        int row1 = -1, row2 = -1, col1 = -1, col2 = -1;
-        if (Character.isLetter(formula.charAt(1))) {
-            col1 = formula.charAt(1) - 'A';
-            row1 = Integer.parseInt(formula.substring(2, idx))-1;
-        }
-        if (Character.isLetter(formula.charAt(idx + 1))) {
-            col2 = formula.charAt(idx + 1) - 'A';
-            row2 = Integer.parseInt(formula.substring(idx + 2))-1;
-        }
         int val1 = 0, val2 = 0;
-        if (row1 != -1)
-            val1 = mat[row1][col1];
-        else
-            val1 = Integer.parseInt(formula.substring(1, idx));
-        if (row2 != -1)
-            val2 = mat[row2][col2];
-        else
-            val2 = Integer.parseInt(formula.substring(idx + 1));
+        String a=formula.substring(1,idx);
+        String b=formula.substring(idx+1);
+        if(mp.containsKey(a)) val1=mp.get(a);
+        else if(!Character.isLetter(a.charAt(0)))
+            val1 = Integer.parseInt(a);
+        if(mp.containsKey(b)) val2=mp.get(b);
+        else if(!Character.isLetter(b.charAt(0)))
+            val2 = Integer.parseInt(b);
         return val1 + val2;
+
     }
 }
 
