d53663c06e78d2100a6aee94428d76042355947c
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-09-21 23:00:46 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (100.00%) - LeetSync

---
+++ 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int evenNumberBitwiseORs(int[] nums) {
+        int res = 0;
+        for (var i : nums) {
+            if ((i % 2) == 0)
+                res = (res | i);
+        }
+        return res;
+    }
+}
