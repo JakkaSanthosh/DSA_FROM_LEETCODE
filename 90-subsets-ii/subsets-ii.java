7a518a68dce18780c11cb9afd0706cee03061e6d
Jakka Santhosh <127023114+JakkaSanthosh@users.noreply.github.com>
2025-10-29 12:30:42 +0530

    Time: 10 ms (6.58%) | Memory: 43.5 MB (79.50%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -2,16 +2,14 @@ class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> outer=new ArrayList<>();
         Arrays.sort(nums);
-        Set<List<Integer>> hs=new HashSet<>();
         int n=nums.length;
         for(int i=0;i<(1<<n);i++){
             List<Integer> inner=new ArrayList<>();
             for(int j=0;j<n;j++){
                 if(checkBit(i,j)) inner.add(nums[j]);
             }
-            hs.add(inner);
+            if(!outer.contains(inner)) outer.add(inner);
         }
-        for(var i:hs) outer.add(i);
         return outer;
     }
     static boolean checkBit(int n,int i){
